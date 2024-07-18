230c9e22c647168f73af3d648a77c671ffecf19b
유문암 <rhyolite199@gmail.com>
2024-07-18 18:40:11 +0900

    FirstProjectFile
    Hello world !

---
+++ Assets/Level1/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float hAxis;
+    float vAxis;
+
+    Vector3 moveVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+    }
+}
